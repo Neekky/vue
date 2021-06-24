baed6ac5fc4041bdcc718432630f6da3eca149ac
cengchao <475383200@qq.com>
2021-06-24 22:13:37 +0800

    feat: 提交

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -29,11 +29,14 @@ export function initGlobalAPI (Vue: GlobalAPI) {
       )
     }
   }
+
+  // 初始化Vue config对象
   Object.defineProperty(Vue, 'config', configDef)
 
   // exposed util methods.
   // NOTE: these are not considered part of the public API - avoid relying on
   // them unless you are aware of the risk.
+  // 这些工具不视作全局Api的一部分，除非你已经意识到某些风险，否则不要去依赖它们
   Vue.util = {
     warn,
     extend,
@@ -41,16 +44,19 @@ export function initGlobalAPI (Vue: GlobalAPI) {
     defineReactive
   }
 
+  // 定义静态方法
   Vue.set = set
   Vue.delete = del
   Vue.nextTick = nextTick
 
   // 2.6 explicit observable API
+  // 让一个对象可响应
   Vue.observable = <T>(obj: T): T => {
     observe(obj)
     return obj
   }
 
+  // 初始化 Vue.options 对象，并给其扩展
   Vue.options = Object.create(null)
   ASSET_TYPES.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
@@ -60,10 +66,15 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
 
+  // 设置keep-alive组件
   extend(Vue.options.components, builtInComponents)
 
+  // 注册Vue.use()用来注册组件
   initUse(Vue)
+  // 注册Vue.mixin()实现混入
   initMixin(Vue)
+  // 注册Vue.extend()基于传入的options返回一个组件的构造函数
   initExtend(Vue)
+  // 注册Vue.directive()、Vue.component()、Vue.filter()
   initAssetRegisters(Vue)
 }
